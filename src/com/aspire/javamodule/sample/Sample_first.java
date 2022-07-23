73eea615ec10efc91976b6ad1b7aa83fc941b4be
AMOL <AMOL@amol>
2022-07-23 17:17:22 +0530

    first push

---
+++ src/com/aspire/javamodule/sample/Sample_first.java
@@ -0,0 +1,10 @@
+package com.aspire.javamodule.sample;
+
+public class Sample_first 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hi this is my first push on git hub");
+	}
+}
