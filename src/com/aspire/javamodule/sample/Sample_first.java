0981b3ba638ead55bb0d5d41936c610e5502313e
AMOL <AMOL@amol>
2022-07-24 10:18:41 +0530

    sample second

--- src/com/aspire/javamodule/sample/Sample_first.java
+++ src/com/aspire/javamodule/sample/Sample_first.java
@@ -6,5 +6,6 @@ public class Sample_first
 	public static void main(String[] args) 
 	{
 		System.out.println("hi this is my first push on git hub");
+		System.out.println("second change in same program");
 	}
 }
